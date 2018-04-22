c2a2861769cb90110bed1613ec91eec5fc881647
good-templates <goodfelib@126.com>
2018-04-22 11:05:15 +0800

    init

---
+++ template/.good.config.js
@@ -0,0 +1,51 @@
+/**
+ * ELF 配置文件
+ *
+ */
+module.exports = {
+  devPort: '8000', // dev server 运行的端口
+  designLayoutWidth: 750, // 设计稿的宽度 | 默认750
+  baseSize: 10, // 计算 rem 的基数，通常不用修改
+  enableREM: true, // 是否用 rem 做适配
+
+  /**
+   * webpack base config
+   */
+  entry: 'src/js/main.js',
+  output: {
+    path: 'dist',
+    publicPath: './',
+    filename: 'js/bundle.js'
+  },
+  outputCss: 'css/app.css',
+  outputCssPublicPath: '../',
+  imgLoaderQuery: {
+    limit: 1000,
+    name: 'img/[name].[ext]'
+  },
+  rules: [{
+    test: /\.js?$/,
+    exclude: /node_modules/,
+    use: [{
+      loader: 'babel-loader',
+      options: {
+        cacheDirectory: true,
+        babelrc: true,
+      }
+    }]
+  }],
+
+  /**
+   * webpack development config
+   *
+   * 只作用于 development 模式，会覆盖 base config 中相同的配置项
+   **/
+  DEVELOPMENT: {},
+
+  /**
+   * webpack production config
+   *
+   * 只作用于 production 模式，会覆盖 base config 中相同的配置项
+   **/
+  PRODUCTION: {}
+};
---
+++ template/src/js/main.js
@@ -0,0 +1,29 @@
+require('../index.html')
+require('../css/package.scss')
+var $ = require('zepto-modules')
+
+class Hello {
+  constructor(name) {
+    this.name = name
+  }
+
+  show () {
+    window.alert(`Hello ${this.name}`)
+  }
+}
+
+const hello = new Hello('Babel')
+
+const $loading = $('#J_loading')
+const $main = $('#J_main')
+const $btn = $('.btn')
+
+$btn.on('touchstart', () => {
+  console.log('touchstart')
+  hello.show()
+})
+
+setTimeout(() => {
+  $loading.remove()
+  $main.removeClass('hide')
+}, 1000)
